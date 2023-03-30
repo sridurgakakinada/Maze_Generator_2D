de8863e58dab67cc81cf5f7019b7cfe92368098b
sridurgakakinada <127333209+sridurgakakinada@users.noreply.github.com>
2023-03-30 20:20:43 +0000

    Initial commit

---
+++ Maze.java
@@ -0,0 +1,10 @@
+public class Maze{
+   private int size;
+   private int[][] nodes;
+   private Graph graph;
+
+   public static void main(String[] args)
+   {
+    System.out.println("Welcome to the Maze!");
+   }
+}
